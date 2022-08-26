220eeb2a7fc779f886048b650d094e62a5f8e6df
Francesco Maccantelli <50288792+macca0612@users.noreply.github.com>
2022-08-26 15:48:07 +0200

    Add files via upload

---
+++ count_car_yolo_02.py
@@ -0,0 +1,171 @@
+# Progetto di Tesi su:
+# Classificazione su base audio del traffico con algoritmi di TinyML su dispositivi embedded
+# Autore: Francesco Maccantelli
+# Data: 20/05/2022
+# Università degli Studi di Siena
+# Software di Calibrazione - Acquisizione dati
+
+
+import cv2 
+#import pandas as pd
+import numpy as np
+import imutils
+
+file_path = "G:/Il mio Drive/AA TESI/script_py/Video_training/high_traffic_day/high_traffic_day.mp4"
+
+#Definizioni pesi traffic_index per categoria
+#PCU: Passenger Car Unit
+weight_car = 1
+weight_bus = 2
+weight_motorcycle = 0.4
+weight_motorbike = 0.4
+weight_truck = 2
+weight_bicycle = 0.2
+
+video_resize = 400    #Resize input video
+
+
+
+def count_veichle(video_path,model,show_bool,cover_box,print_found):
+    traffic_index = 0
+
+    cap = cv2.VideoCapture(video_path)
+
+    if not cap.isOpened():
+        print("Video not opened correctly")
+        print("ERROR ON VIDEO: ",video_path)
+        return None
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        
+        #frame = imutils.resize(frame,640,480)
+        
+        #censura
+        if not ret:
+            #print("Can't receive frame (stream end?). Exiting ...")
+            break
+
+        #frame = imutils.resize(frame,video_resize) #Resizing input video
+
+        h_frame, w_frame, channels = frame.shape
+        if (cover_box):
+            
+            #contours1 = np.array( [ [0,200], [w_frame,h_frame-50], [w_frame, h_frame], [0,h_frame] ] )
+
+            #contours2 = np.array( [ [w_frame,0], [w_frame,318], [395,245],[421, 0]  ] )
+
+            #contours3 = np.array( [ [0,160], [411,130], [420,0],[0, 0]  ] )
+
+            pts  = np.array([[0, 308], [503, 460], [503, 480], [0, 480]])
+            pts2 = np.array([[0, 260], [465, 210], [479, 0], [0, 0]])
+            pts3 = np.array([[453, 358], [477, 0], [640, 0], [640, 408]])
+            
+            
+            
+            
+            
+
+            #contours3 = np.array( [[w_frame * 0.623, h_frame * 0.508], [w_frame * 0.653, 0], [w_frame, 0], [w_frame, h_frame * 0.658]])
+
+            cv2.fillPoly(frame, pts =[pts], color=(0,0,0))
+            cv2.fillPoly(frame, pts =[pts2], color=(0,0,0))
+            cv2.fillPoly(frame, pts =[pts3], color=(0,0,0))
+        
+        # if frame is read correctly ret is True
+        if not ret:
+            #print("Can't receive frame (stream end?). Exiting ...")
+            break
+        
+        results = model(frame)
+
+        df = results.pandas().xyxy[0]
+        #print(df)
+        car_found = (df.name.values == "car").sum()
+        bus_found = (df.name.values == "bus").sum()
+        motorcycle_found = (df.name.values == "motorcycle").sum()
+        motorbike_found = (df.name.values == "motorbike").sum()
+        truck_found = (df.name.values == "truck").sum()
+        bicycle_found = (df.name.values == "bicycle").sum()
+
+        traffic_index = traffic_index + (car_found * weight_car) + (bus_found * weight_bus) + (motorcycle_found * weight_motorcycle) + (truck_found * weight_truck) + (bicycle_found * weight_bicycle)
+
+        if (print_found):
+            if (car_found > 0 ):
+                print("cars: ",car_found)
+            if (bus_found > 0 ):
+                print("bus: ",bus_found)
+            if (motorcycle_found > 0 ):
+                print("motorcycle: ",motorcycle_found)
+            if (truck_found > 0 ):
+                print("truck: ",truck_found)
+            if (motorbike_found > 0 ):
+                print("motorbike: ",motorbike_found)
+            if (bicycle_found > 0 ):
+                print("bicycle: ",bicycle_found)
+
+
+        if (show_bool):
+            for index, row in df.iterrows():
+                x_min = int(row['xmin'])
+                y_min = int(row['ymin'])
+                x_max = int(row['xmax'])
+                y_max = int(row['ymax'])
+                name = str(row['name'])
+                conf = round(row['confidence'],2)
+                conf = str(conf)
+                w = x_max - x_min
+                h = y_max - y_min
+
+                if (name == "car"):
+                    color = [255,0,0]
+                elif (name == "truck"):
+                    color = [0,255,0]
+                elif (name == "bus"):
+                    color = [0,0,255]
+                elif (name == "motorbike"):
+                    color = [255,255,0] 
+                elif (name == "motorcycle"):
+                    color = [0,255,255] 
+                elif (name == "person"):
+                    color = [125,255,125]
+                elif (name == "bicycle"):
+                    color = [125,0,255]
+                else:
+                    color = [0,0,0]
+
+                cv2.rectangle(frame,(x_min,y_min), (x_min+w, y_min+h), color, (2))
+                cv2.putText(frame, name, (x_max,y_min), cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 2)
+                cv2.putText(frame, conf, (x_max,y_max), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255), 1)
+                text_t_i = "T I: " + str(traffic_index)
+                cv2.putText(frame, text_t_i, (0,h_frame-30), cv2.FONT_HERSHEY_PLAIN, 3, (125,125,125), 3 )
+
+            cv2.imshow('frame', frame)
+            
+            if cv2.waitKey(1) == ord('q'):
+                break
+               
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+    return traffic_index
+
+
+
+
+def main():
+    import torch
+    # Modello YOLO
+    url = "G:/Il mio Drive/AA TESI/win_code/analisi/analisi_06_22_08_21/video/20220622-110041.avi"
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5l')  # or yolov5n - yolov5x6, custom
+    _ = count_veichle(url,model,True,True,False)
+    #                           Show,Cover,Print
+
+if __name__ == "__main__":
+    main()
+
+
+
+
---
+++ main_analisi.py
@@ -0,0 +1,580 @@
+# Progetto di Tesi su:
+# Classificazione su base audio del traffico con algoritmi di TinyML su dispositivi embedded
+# Autore: Francesco Maccantelli
+# Data: 20/05/2022
+# Università degli Studi di Siena
+# Software per creazione TrainingSet - Main Analisi
+
+
+from ast import operator
+from select import select
+from socket import timeout
+from cv2 import threshold
+import count_car_yolo_02                         #script to analyze video and get traffic_index
+from matplotlib import pyplot as plt    #lilbrary to plot
+import os       #library for scroll to all file
+import torch    #library for YOLO
+import time     #library for time
+#import shutil   #limbrary for copy/paste
+import serial
+import cv2
+from tqdm import tqdm
+import numpy as np
+from scipy import interpolate
+import tkinter as tk
+from tkinter import filedialog
+
+
+
+def draw_black_box(path_video):
+    video = cv2.VideoCapture(path_video)
+    _ , frame = video.read()
+
+    width  = video.get(3)  # float `width`
+    height = video.get(4)  # float `height`
+
+    list_position_past = []
+    list_list = []    
+
+    def draw(event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            print("set")
+            cv2.circle(frame,(x,y),5,(0,255,0),-1)
+
+            if x < 10:
+                x=0
+            if y < 10:
+                y=0
+            if x > (width-10):
+                x = int(width)
+            if y > (height-10):
+                y = int(height)
+
+            list_position_past.append([x,y])
+            if (len(list_position_past) == 4):
+                list_list.append(list_position_past)
+                print(list_position_past)
+                list_position_past.clear()
+                
+            
+
+    cv2.namedWindow(winname= "Title of Popup Window")
+    cv2.setMouseCallback("Title of Popup Window", draw)
+
+    while True:
+        cv2.imshow("Title of Popup Window", frame)
+        if cv2.waitKey(10) & 0xFF == 27:
+            break
+    cv2.destroyAllWindows()
+
+
+# function to analyze video and save t_i on file (generated automaticaly by year-month-day-hour-minutes-seconds)
+
+def video_analyze(path_to_analyze):
+
+    print("Folder: ",path_to_analyze)
+    video_folder = path_to_analyze + "/video"
+    audio_folder = path_to_analyze + "/audio"
+
+    #import YOLO model
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5n - yolov5x6, custom
+
+    my_data_prefix = time.strftime("%Y%m%d_%H%M%S")
+
+    saving_path = path_to_analyze+"/save_"+my_data_prefix+".txt"
+
+    saving = open(saving_path,"a")
+
+    list_t_i = []
+
+    #scroll to all video file
+    for file in tqdm(os.listdir(video_folder)):
+        path_2_video = video_folder+"/"+file
+                                              #path_video #yolo #show#cover#print_found
+
+        t_i = count_car_yolo_02.count_veichle(path_2_video,model,False,True,False)
+
+        if t_i == None:
+            continue
+
+        list_t_i.append(t_i)
+
+        saving.write(file + " " + str(t_i) + "\n")
+        saving.flush()
+    
+    saving.close()
+
+
+def video_test(path_to_analyze):
+
+    video_folder = path_to_analyze + "/video"
+
+    #import YOLO model
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5l')  # or yolov5n - yolov5x6, custom
+  
+
+    #scroll to all video file
+    for file in tqdm(os.listdir(video_folder)):
+        path_2_video = video_folder+"/"+file
+                                              #path_video #yolo #show#cover#print_found
+        t_i = count_car_yolo_02.count_veichle(path_2_video,model,True,True,False)
+
+
+# Function to print graph from save file   
+
+def print_graph_from_file(path_file):
+
+    list_file_name = []
+    list_t_i = []
+    
+    
+
+    with open(path_file) as file:
+        lines = file.readlines()
+
+    
+
+    for line in tqdm(lines):
+        list_file_name.append(line[:19])
+        t_i = float(line[20:][:-1])
+        #t_i = float(line[11:][:-1])
+        list_t_i.append(t_i)
+        
+        
+
+    x=range(len(list_t_i))
+
+    plt.plot(x,list_t_i,color='green', linestyle='solid', linewidth = 1,
+         marker='o', markerfacecolor='blue', markersize=3)
+    plt.legend( [ 'Traffic_index' ] )
+    plt.show()
+
+
+# Function to export list_file_name and list_t_i
+
+def Get_list_name_t_i_fromFile(path_save_file):
+
+    list_file_name = []
+    list_t_i = []
+
+    with open(path_save_file) as file:
+        lines = file.readlines()
+
+    for line in lines:
+        list_file_name.append(line[:19])
+        t_i = float(line[20:][:-1])
+        list_t_i.append(t_i)
+    
+    return list_file_name , list_t_i
+
+def Get_list_name_t_i_class_fromFile(path_save_File):
+
+    list_name = []
+    list_t_i = []
+    list_class = []
+
+    with open(path_save_File) as file:
+        lines = file.readlines()
+    
+    for line in lines:
+        list_name.append(line[:19])
+        list_t_i.append(line[20:-2])
+        list_class.append(line[-2:][:1])
+
+    # print ("name" + str(list_name))
+    # print ("t_i" + str(list_t_i))
+    # print ("class" + str(list_class))
+
+    return list_name, list_t_i, list_class
+
+
+# Function to export audio file based on thresholds [assente,presente,intenso]
+
+
+def export_audio_file(path_save_file,thresholds):
+
+    try:
+        th_presente = thresholds[0]
+        print("Th LOW = ",th_presente)
+        th_intenso = thresholds[1]
+        print("Th HIGH = ",th_intenso)
+    except:
+        print("ERROR! - threshold list not set propriely, must be a 2 dimension list with numbers e.g. [125.5,500]")
+        quit()
+
+    base_path_audio = path_save_file[:-24]+"export"
+    base_path_export = path_save_file[:-24]+"export"
+
+    export_audio_log = base_path_export + "/export_audio_log.txt"
+    audio_log = open(export_audio_log,"a")
+
+    list_file_name , list_t_i = Get_list_name_t_i_fromFile(path_save_file)
+
+    for i in tqdm(range(len(list_file_name))):
+        t_i_selected = list_t_i[i]
+        name_selected = list_file_name[i]
+
+        if t_i_selected < th_presente:
+            prefix = "ASSENTE."
+        elif t_i_selected < th_intenso:
+            prefix = "PRESENTE."
+        else:
+            prefix = "INTENSO."
+
+        audio_log.write(str(name_selected) + " " + str(t_i_selected) + " " + str(prefix[0])+ "\n")
+        #audio_log.flush()
+
+        path_selected =  base_path_audio + "/" + name_selected[:-4] + ".json"
+        path_destination = base_path_export + "/" + prefix + str(t_i_selected) + "." + name_selected[:-4] + ".json"
+
+        #shutil.copyfile(path_selected, path_destination)
+        try:
+            os.rename(path_selected, path_destination)
+        except:
+            print("ERRORE ! - file: ",path_selected," NON TROVATO")
+
+    audio_log.close()
+
+
+def check_export(path_export_file):
+
+    base_folder = path_export_file[:-27]
+    list_name, list_t_i, list_class = Get_list_name_t_i_class_fromFile(path_export_file)
+
+    selection = input("Press I: Intenso, P: Presente, A: Assente")
+    if not selection == ("I" or "P" or "A"):
+        print("Selezione non valida")
+        return None
+
+    video_folder = base_folder + "/video"
+
+    for i in range(len(list_name)):
+        print(list_class[i])
+        if list_class[i] == selection:
+            Play_video(video_folder+"/"+str(list_name[i]))
+        else:
+            continue
+        val = input("The video is " + selection + " ? : [y/n/r:repeat] :")
+       
+        if val == "y" or "Y" or "yes" or "Yes":
+            continue
+        else:
+            new_class = input("Select classification [I,P,A]: ")
+
+
+
+
+
+
+def VideoTesting(webcam_number,serial_port,video_path,fps,record_time):
+    webcam = cv2.VideoCapture(webcam_number,cv2.CAP_DSHOW)
+    input_serial = serial.Serial(serial_port,115200,timeout=None)
+
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+    video_out = cv2.VideoWriter(video_path,fourcc,fps,(640,480))
+
+    start_time = time.time()
+    print("Starting recording...")
+
+    read_serial_1 = str("1")
+    read_serial_2 = str("2")
+    read_serial_3 = str("3")
+    read_serial_4 = str("4")
+    color_assente = (255,255,255)
+    color_presente = (255,255,255)
+    color_intenso = (255,255,255)
+    number_classification = 0
+    list_past_classification = ["Prestene","Intenso","Assente"]
+
+    while(True):
+        # reads frames from a camera
+        # ret checks return at each frame
+        ret, frame = webcam.read()
+        
+        # output the frame
+        
+        pause_time_start = time.time()
+
+        
+        if(input_serial.inWaiting()>1):
+            number_classification = number_classification +1       
+            read_serial_1 = str(input_serial.readline())[2:-3]
+            read_serial_2 = str(input_serial.readline())[6:-3]
+            value_assente = float(read_serial_2[9:])
+            if value_assente > 0.5:
+                color_assente = (0,255,0)
+                list_past_classification.append("Assente")
+            else:
+                color_assente = (255,0,0)
+            
+            read_serial_3 = str(input_serial.readline())[6:-3]
+            value_intenso = float(read_serial_3[9:])
+            if value_intenso > 0.5:
+                color_intenso = (0,255,0)
+                list_past_classification.append("Intenso")
+            else:
+                color_intenso = (255,0,0)
+           
+            read_serial_4 = str(input_serial.readline())[6:-3]
+            value_presente = float(read_serial_4[9:])
+            if value_presente > 0.5:
+                color_presente = (0,255,0)
+                list_past_classification.append("Presente")
+            else:
+                color_presente = (255,0,0)
+            
+            input_serial.reset_input_buffer()
+        
+        """ read_serial_1 = str("1")
+        read_serial_2 = str("2")
+        read_serial_3 = str("3")
+        read_serial_4 = str("4") """
+
+        cv2.putText(frame,read_serial_1,(30,300), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,0), 1)
+        cv2.putText(frame,read_serial_2,(30,350), cv2.FONT_HERSHEY_PLAIN, 2, color_assente, 2)
+        cv2.putText(frame,read_serial_3,(30,400), cv2.FONT_HERSHEY_PLAIN, 2, color_intenso, 2)
+        cv2.putText(frame,read_serial_4,(30,450), cv2.FONT_HERSHEY_PLAIN, 2, color_presente, 2)
+        cv2.putText(frame,list_past_classification,(30,470), cv2.FONT_HERSHEY_PLAIN, 2, color_presente, 2)
+        cv2.putText(frame,"# class:"+str(number_classification),(475,450), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,255), 1)
+
+        video_out.write(frame)
+        cv2.imshow('webcam', frame)
+        if cv2.waitKey(1) == ord('q'):
+                break
+
+        #print(read_serial_1,read_serial_2,read_serial_3,read_serial_4)
+
+    webcam.release()
+    video_out.release()
+    cv2.destroyAllWindows()
+
+# Open selected file based x progression
+
+def open_video_file(path_save_file):
+
+    while True:
+        try:
+            select = int(input("Select index for video: "))
+        except:
+            print("Index must be a number")
+            continue
+
+        if select == "q" :
+            break
+
+        list_file_name, _ = Get_list_name_t_i_fromFile(path_save_file)
+        path_folder = path_save_file[:-25]
+        # print("Path folder > ",path_folder)
+        video_folder = path_folder + "/video"
+        # print("Video folder > ",video_folder)
+        try:
+            video_path = video_folder + "/" +list_file_name[select]
+        except:
+            print("Index not correct")
+            continue
+        print("Video path > ",video_path)
+        # Create a VideoCapture object and read from input file
+        try:
+            cap = cv2.VideoCapture(video_path)
+        except:
+            print("File not found")
+            continue
+        
+        # Check if camera opened successfully
+        if (cap.isOpened()== False): 
+            print("Error opening video  file")
+        
+        # Read until video is completed
+        while(cap.isOpened()):
+            
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+            if ret == True:
+            
+                # Display the resulting frame
+                cv2.imshow('Frame', frame)
+            
+                # Press Q on keyboard to  exit
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                    break
+            
+            # Break the loop
+            else: 
+                break
+    
+        # When everything done, release 
+        # the video capture object
+        cap.release()
+        
+        # Closes all the frames
+        cv2.destroyAllWindows()
+
+
+def Play_video(video_path):
+    try:
+        Video_stream = cv2.VideoCapture(video_path)
+    except:
+        print("File not found")
+        return None
+        
+        
+    # Check if camera opened successfully
+    if (Video_stream.isOpened()== False): 
+        print("Error opening video  file")
+    
+    # Read until video is completed
+    while(Video_stream.isOpened()):
+        
+        # Capture frame-by-frame
+        ret, frame = Video_stream.read()
+        if ret == True:
+        
+            # Display the resulting frame
+            cv2.imshow('Frame', frame)
+        
+            # Press Q on keyboard to  exit
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+        
+        # Break the loop
+        else: 
+            break
+
+    # When everything done, release 
+    # the video capture object
+    Video_stream.release()
+    
+    # Closes all the frames
+    cv2.destroyAllWindows()
+
+
+
+
+#export_audio_file("G:/Il mio Drive/AA TESI/win_code/analisi/analisi_test/save_20220620_214706.txt",[690,700])
+#print_graph_from_file("G:/Il mio Drive/AA TESI/win_code/analisi/analisi_test/save_20220620_214706.txt")
+#video_analyze()
+
+def main():
+    print("""   _____        __ _                                                 _           _     
+  / ____|      / _| |                              /\               | |         (_)    
+ | (___   ___ | |_| |___      ____ _ _ __ ___     /  \   _ __   __ _| |_   _ ___ _ ___ 
+  \___ \ / _ \|  _| __\ \ /\ / / _` | '__/ _ \   / /\ \ | '_ \ / _` | | | | / __| |/ __|
+  ____) | (_) | | | |_ \ V  V / (_| | | |  __/  / ____ \| | | | (_| | | |_| \__ | |\__ /
+ |_____/ \___/|_|  \__| \_/\_/ \__,_|_|  \___| /_/    \_\_| |_|\__,_|_|\__, |___/_|___/
+                                                                        __/ |          
+                                                                       |___/           """)
+    print("for thesis :")
+    print("Classificazione su base audio del traffico con algoritmi di TinyML su dispositivi embedded")
+    print("made by:")
+    print("Francesco Maccantelli")
+    print("Universitá degli Studi di Siena - 20/05/2022")
+
+    while True:
+
+        print("############################################")
+        print("Select function:\n1) Video analysys:\n[path_folder_to_analyze] (Attention! in this folder must be almost 3 folders audio,video,export)")
+        print("2) Print grph from save file\n[path_save_file]\n3) Export audio file based on threshold\n[path_save_file,list_threshold] (Attention! list threshold must be 2 dimension list with numbers, like [50,500])")
+        print("4) Draw black box")
+        print("5) Video Testing")
+        print("6) Video test YOLO")
+        print("7) Play video")
+        print("8) check export")
+        print("9) Exit")
+        selection = input("\n------------------------------------------\nSelect function [1,2,3,4,5,6,7,8] :")
+
+        if selection == "1":
+            print("## Video analysys ##")
+            
+            print("Selecet folder to analyze")
+
+            root = tk.Tk()
+            root.withdraw()
+
+            path_to_analyze = filedialog.askdirectory()
+
+            #JUST FOR TESTING PURPUSE
+            if path_to_analyze == "test":
+                path_to_analyze = "G:/Il mio Drive/AA TESI/win_code/analisi/analisi_test"
+
+            video_analyze(path_to_analyze)
+        elif selection == "2":
+            print("## Print grph from save file ##")
+
+            root = tk.Tk()
+            root.withdraw()
+            print("Select Save File")
+            path_save_file = filedialog.askopenfilename()
+
+
+            print_graph_from_file(path_save_file)
+        elif selection == "3":
+            print("## Export audio file based on threshold ##")
+
+            root = tk.Tk()
+            root.withdraw()
+            print("Select Save File")
+            path_save_file = filedialog.askopenfilename()
+
+            threshold = []
+            first_threshold = input("Insert FRST threshold :")
+            threshold.append(float(first_threshold))
+            second_threshold = input("Insert SECOND threshold :")
+            threshold.append(float(second_threshold))
+            export_audio_file(path_save_file,threshold)
+        elif selection == "4":
+            print("Select video for reference")
+            root = tk.Tk()
+            root.withdraw()
+            path_video = filedialog.askopenfilename()
+            draw_black_box(path_video)
+
+        elif selection =="5":
+            webcam_number = 1
+            serial_port = "COM3"
+
+            root = tk.Tk()
+            root.withdraw()
+
+            video_path = filedialog.askdirectory()
+            video_path = video_path + "/video_analysis_" + time.strftime("%Y%m%d_%H%M%S") + ".avi"
+            fps = 30.0
+            record_time = 15
+            VideoTesting(webcam_number,serial_port,video_path,fps,record_time)
+
+        elif selection == "6":
+            print("## Video test ##")
+            
+            print("Selecet folder to analyze")
+
+            root = tk.Tk()
+            root.withdraw()
+
+            path_to_analyze = filedialog.askdirectory()
+
+            video_test(path_to_analyze)
+
+        elif selection == "7":
+            print("Selecte save file")
+            root = tk.Tk()
+            root.withdraw()
+            path_save_file = filedialog.askopenfilename()
+            print("press Q to exit")
+            open_video_file(path_save_file)
+
+        elif selection == "8":
+            print("Selecte export file")
+            root = tk.Tk()
+            root.withdraw()
+            path_export_file = filedialog.askopenfilename()
+            check_export(path_export_file)
+
+        elif selection == "9" or "q" or "Q" or "quit" or "QUIT":
+            print("Sono dentro qua!")
+            print("Quitting...")
+            quit()
+        else:
+            print("ERROR! - selection not valid")
+
+if __name__ == "__main__":
+    main()
